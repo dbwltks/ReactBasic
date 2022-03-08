ab4ac3204c6916146deca4dc372d30a3c4d98e50
Jisan Yu <dbwltks@gmail.com>
2022-03-08 21:52:23 +0900

    React 준비

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 // 회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
@@ -37,7 +38,7 @@ app.post("/register", (req, res) => {
   }); // mongodb명령어
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 찾기
   const user = new User(req.body);
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -65,7 +66,29 @@ app.post("/login", (req, res) => {
       });
     });
   });
-  // 비밀번호까지 같다면 Token 생성
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication = true
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.images,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,7 @@ const userSchema = mongoose.Schema({
 //mongo메서드, 저장하기전에 비밀번호를 암호화
 //salt를 이용해서 암호화, salt생성, saltRounds 10자리
 userSchema.pre("save", function (next) {
-  var user = this;
+  const user = this;
   if (user.isModified("password")) {
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
@@ -62,13 +62,27 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 };
 
 userSchema.methods.generateToken = function (cb) {
-  var user = this;
+  const user = this;
   // jsonwebtoken을 이용해서 token을 생성
-  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
   user.token = token;
   user.save(function (err, user) {
     if (err) return cb(err);
     cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+
+  // 토큰 복호화
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    // 유저아이디를 이용해서 유절르 찾고 클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰이 일치하는지 확인
+
+    user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
   });
 };
 
